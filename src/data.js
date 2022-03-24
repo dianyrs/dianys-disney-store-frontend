5b1ae715a40ef4771238a6f7c4cf7eed5671cce4
diany <dianysecrets5.0@gmail.com>
2022-03-24 02:03:10 -0400

    more changes in the front

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://cdn-ssl.s7.disneystore.com/is/image/DisneyShopping/2841041618986?fmt=webp&qlt=70&wid=643&hei=643",
+        title: "CUSTOME SALE",
+        desc: "GET 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://cdn-ssl.s7.disneystore.com/is/image/ShopDisney/4up-sq_clothing-boy_20220228_2x?fit=constrain&cropN=0,0,1,1&fmt=webp&qlt=70&wid=770",
+        title: "Kids' Clothing",
+        desc: "GET 20% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",
+    },
+    {
+        id: 3,
+        img: "https://cdn-ssl.s7.disneystore.com/is/image/ShopDisney/4up-sq_clothing-men_20220228_2x?fit=constrain&cropN=0,0,1,1&fmt=webp&qlt=70&wid=770",
+        title: "ADULTS SALE",
+        desc: "GET 30% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafi",
+    }
+]
